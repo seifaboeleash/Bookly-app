96b70c2c39ccfedc7e971460a64b828d68a336d4
Seif Abo Eleash <seifaboeleish@gmail.com>
2025-09-21 16:13:54 +0300

    refactor initstate $ navigate home

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,9 @@
 import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -9,58 +12,70 @@ class SplashViewBody extends StatefulWidget {
   State<SplashViewBody> createState() => _SplashViewBodyState();
 }
 
-class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin {
-  late AnimationController animationController; 
-  late Animation<Offset> slidingAnimation ;
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
 
   @override
   void initState() {
-    animationController = AnimationController(
-      vsync: this ,
-       duration: Duration(seconds: 1));
-
-    slidingAnimation = Tween<Offset>(
-      begin: Offset(0, 2),
-      end: Offset.zero,
-    ).animate(animationController);
-    
-    slidingAnimation.addListener((){
-      setState(() {
-        
-      });
-    });
-    animationController.forward();
+    initSlideAnimation();
     super.initState();
+
+    NavigateToHome();
   }
+
   @override
   void dispose() {
     animationController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
-    return  Column(
+    return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        SvgPicture.asset(kLogo , width: 200,),
-        const SizedBox(
-          height: 5,
-        ),
+        SvgPicture.asset(kLogo, width: 200),
+        const SizedBox(height: 5),
         AnimatedBuilder(
           animation: slidingAnimation,
-          builder: (context , _) {
+          builder: (context, _) {
             return SlideTransition(
               position: slidingAnimation,
-              child: const Text(
-                'Read Free Books',
-              textAlign: TextAlign.center,
-              ),
+              child: const Text('Read Free Books', textAlign: TextAlign.center),
             );
-          }
-        )
-        
+          },
+        ),
       ],
     );
+  }
+
+  void initSlideAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+
+    slidingAnimation = Tween<Offset>(
+      begin: Offset(0, 2),
+      end: Offset.zero,
+    ).animate(animationController);
+
+    slidingAnimation.addListener(() {
+      setState(() {});
+    });
+    animationController.forward();
+  }
+
+  void NavigateToHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(
+        () => HomeView(),
+        transition: Transition.fade,
+        duration: const Duration(microseconds: 250),
+      );
+    });
   }
 }
